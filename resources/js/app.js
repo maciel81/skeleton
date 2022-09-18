50dd54fc42298d5aea6ea703d9dba0ad10a46606
Maciel81 <emeoitoum@gmail.com>
2022-09-17 22:19:50 -0300

    Selectize instalado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+// jQuery
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+// Selectize
+import '@selectize/selectize/dist/js/standalone/selectize';
+
+$(document).ready(function() {
+    $(".selectize").selectize();
+});
